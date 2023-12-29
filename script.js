93daddfafd2c0c39fd618a652dcecd9ea18655a9
Pedro Matos <109925892+pedrodabahia@users.noreply.github.com>
2023-12-29 15:20:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 var i = 0;
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById('inicialize').style.display = "block";
+})
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
   
@@ -32,12 +35,35 @@ app.controller('myCtrl', function($scope) {
 
 
   $scope.pessoas = []
+  $scope.table1 = "Table-1"
+  $scope.table2 = "Tabela-2"
+  $scope.table3 = "Tabela-3"
+
+  $scope.inicializador = function(){
+    $scope.table1 = $scope.editTable1.toUpperCase();
+    $scope.table2 = $scope.editTable2.toUpperCase();
+    $scope.table3 = $scope.editTable3.toUpperCase();
+    document.getElementsByName('input-column1')[0].placeholder = $scope.editTable1.toUpperCase();
+    document.getElementsByName('input-column1')[1].placeholder = $scope.editTable1.toUpperCase();
+    document.getElementsByName('input-column2')[0].placeholder = $scope.editTable2.toUpperCase();
+    document.getElementsByName('input-column2')[1].placeholder = $scope.editTable2.toUpperCase();
+    document.getElementsByName('input-column3')[0].placeholder = $scope.editTable3.toUpperCase();
+    document.getElementsByName('input-column3')[1].placeholder = $scope.editTable3.toUpperCase();
+    document.getElementById('inicialize').style.display = "none";
+  }
+
 
   $scope.adiciona = function(){
-    $scope.add = {"nome": $scope.addNome.toUpperCase() , "cpf":$scope.addCpf.toUpperCase(),"cidade":$scope.addCidade.toUpperCase()}
+    var elemento1 = $scope.table1;
+    var elemento2 = $scope.table2;
+    var elemento3 = $scope.table3;
+    $scope.add = { elemento1 : $scope.addValor1.toUpperCase() , elemento2 : $scope.addValor2.toUpperCase(),elemento3 : $scope.addValor3.toUpperCase()}
     $scope.pessoas.push($scope.add);
     document.getElementById('formulario').classList.toggle('fade');
     document.getElementById('iconCad').classList.toggle('rotate');  
+    $scope.addValor1 = ""
+    $scope.addValor2 = ""
+    $scope.addValor3 = ""
   }
 
 
@@ -47,8 +73,12 @@ app.controller('myCtrl', function($scope) {
 
 
     $scope.editar = function(){
-        $scope.pessoas[$scope.id] = {"nome": $scope.EditNome.toUpperCase() , "cpf":$scope.EditCpf.toUpperCase(),"cidade":$scope.EditCidade.toUpperCase()}
-        document.getElementById('editor-div').classList.toggle('fade')
+        $scope.pessoas[$scope.id] = {elemento1 : $scope.EditValor1.toUpperCase() , elemento2 :$scope.EditValor2.toUpperCase(),elemento3 :$scope.EditValor3.toUpperCase()}
+        document.getElementById('editor-div').classList.toggle('fade');
+        $scope.EditValor1 = ""
+        $scope.EditValor2 = ""
+        $scope.EditValor3 = ""
+    
     }
 
 
@@ -62,17 +92,16 @@ app.controller('myCtrl', function($scope) {
       var data = [];
       for(let i = 0;i < $scope.pessoas.length;i++){   
       
-      data[i] = "\n Nome: "+ $scope.pessoas[i].nome+"\n"+"Cpf: "+ $scope.pessoas[i].cpf+"\n"+"Cidade: "+ $scope.pessoas[i].cidade+"\n";
+     data[i] = `\n ${$scope.table1}: `+ $scope.pessoas[i].elemento1+"\n"+` ${$scope.table2}: `+ $scope.pessoas[i].elemento2 +"\n"+`${$scope.table3}: `+ $scope.pessoas[i].elemento3 +"\n";
     }
 
-      download(data,`Relatorio_${i++}`,'txt');}
+      download(data,`TableGenerator_${i++}`,'txt');}
   });
 
   document.getElementById("iconCad").addEventListener('click',() => {
     document.getElementById('formulario').classList.toggle('fade');
     document.getElementById('iconCad').classList.toggle('rotate');
   })
-  document.getElementById("close").addEventListener('click',() => {
+  document.getElementById("close-edit").addEventListener('click',() => {
     document.getElementById('editor-div').classList.toggle('fade');
   })
-  
